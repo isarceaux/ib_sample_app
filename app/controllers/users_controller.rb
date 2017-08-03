bd7580991f80afe1572334eefd225a44274e7075
isarceaux <isarceaux>
2017-08-03 13:47:01 +0200

    Finished chapter 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -55,17 +56,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-# Before filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
+    # Before filters
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
